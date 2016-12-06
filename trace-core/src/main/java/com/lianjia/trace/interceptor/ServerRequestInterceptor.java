435371cccae065ea6f11fc0ef9b4f02248de0b38
libaoyin <libaoyin@lianjia.com>
2016-12-06 21:13:07 +0800

    ME

--- trace-core/src/main/java/com/lianjia/trace/interceptor/ServerRequestInterceptor.java
+++ trace-core/src/main/java/com/lianjia/trace/interceptor/ServerRequestInterceptor.java
@@ -1,7 +1,5 @@
 package com.lianjia.trace.interceptor;
 
-import java.util.logging.Logger;
-
 import com.lianjia.trace.KeyValueAnnotation;
 import com.lianjia.trace.Span;
 import com.lianjia.trace.TraceData;
@@ -10,8 +8,6 @@ import com.lianjia.trace.tracer.ServerTracer;
 import com.lianjia.trace.util.Util;
 
 public class ServerRequestInterceptor {
-	private final static Logger LOGGER = Logger.getLogger(ServerRequestInterceptor.class.getName());
-
 	private final ServerTracer serverTracer;
 
 	public ServerRequestInterceptor(ServerTracer serverTracer) {
@@ -24,14 +20,11 @@ public class ServerRequestInterceptor {
 		Boolean sample = traceData.getSample();
 		if (sample != null && Boolean.FALSE.equals(sample)) {
 			serverTracer.setStateNoTracing();
-			LOGGER.fine("Received indication that we should NOT trace.");
 		} else {
 			if (traceData.getSpan() != null) {
-				LOGGER.fine("Received span information as part of request.");
 				Span span = traceData.getSpan();
 				serverTracer.setStateCurrentTrace(span.getTraceId(), span.getId(), span.getParentId(), span.getName());
 			} else {
-				LOGGER.fine("Received no span state.");
 				serverTracer.setStateUnknown(adapter.getSpanName());
 			}
 			serverTracer.setServerReceived();
